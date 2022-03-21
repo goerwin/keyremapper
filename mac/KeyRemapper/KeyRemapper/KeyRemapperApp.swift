b8f9d38e83a28ec491839a4f1ef53f650e6c0011
Erwin G <erwingaitano@gmail.com>
2022-03-20 20:49:49 -0500

    using config1 file instead of mode

--- mac/KeyRemapper/KeyRemapper/KeyRemapperApp.swift
+++ mac/KeyRemapper/KeyRemapper/KeyRemapperApp.swift
@@ -26,33 +26,34 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
       if let window = NSApplication.shared.windows.first { window.close() }
 
       createMenuBar()
-      
+
       guard let rootPath = getRootPath() else {
         return showCloseAlert("Error", "Config Folder invalid")
       }
-      
+
       if (!isAppAccesibilityEnabled()) {
         showCloseAlert("Enable Accesibility", "Enable Accesibility for this app")
         IOHIDRequestAccess(kIOHIDRequestTypePostEvent)
         return;
       }
-      
+
       let symbolsPath = "\(rootPath)/symbols.json"
       if (!fileExists(symbolsPath)) {
         return showCloseAlert("File not found", "\(symbolsPath) not found")
       }
-      
+
       let config1Path = "\(rootPath)/config1.json"
       if (!fileExists(config1Path)) {
         return showCloseAlert("File not found", "\(config1Path) not found")
       }
-      
+
       showNotification("Config loaded from: \(config1Path)", nil)
-//      sendNotification(selectedMode + " selected" + testsResultsMsg + "\n" + "Config files in: " + rootPathStr);
+      // TODO: Show test results
+//      sendNotification(selectedConfig + " selected" + testsResultsMsg + "\n" + "Config files in: " + rootPathStr);
       self.rootPath = rootPath
-//      startBgProcess(configPath: config1Path, symbolsPath: symbolsPath)
+      startBgProcess(configPath: config1Path, symbolsPath: symbolsPath)
     }
-  
+
   func fileExists(_ filePath: String) -> Bool {
       let fileManager = FileManager.default
       if fileManager.fileExists(atPath: filePath) {
@@ -61,7 +62,7 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
 
       return false
   }
-  
+
   func showNotification(_ description: String, _ title: String?) {
     let center = UNUserNotificationCenter.current()
     let options: UNAuthorizationOptions = [.alert, .badge]
@@ -70,17 +71,17 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
           let content = UNMutableNotificationContent()
           content.title = title ?? bundleName
           content.body = description
-  
+
     let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false);
 
           let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-  
+
           center.add(request)
         }
     }
-    
+
   }
-  
+
   func showCloseAlert(_ title: String, _ description: String) {
           let alert = NSAlert()
           alert.messageText = title
@@ -89,7 +90,7 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
           alert.addButton(withTitle: "OK")
     alert.runModal()
    }
-  
+
   func isAppAccesibilityEnabled() -> Bool {
     let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString: false]
     return AXIsProcessTrustedWithOptions(options as CFDictionary)
@@ -100,7 +101,7 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
       self.background = Background(configPath: configPath, symbolsPath: symbolsPath)
       }
   }
-  
+
   func resizeImage(image: NSImage, w: Int, h: Int) -> NSImage? {
     let frame = NSRect(x: 0, y: 0, width: w, height: h)
     let newSize = NSSize(width: w, height: h)
@@ -116,11 +117,11 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
   }
 
   private var aboutBoxWindowController: NSWindowController?
-  
+
   func createMenuBar() {
     statusBarMenu = NSMenu(title: "Cap Status Bar Menu")
     statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-    
+
     let menuButton = statusBarItem.button!
     menuButton.title = " Config 1"
     menuButton.imagePosition = NSControl.ImagePosition.imageLeft
@@ -139,33 +140,33 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
         withTitle: "About \(bundleName)",
         action: #selector(AppDelegate.openAboutWindow),
         keyEquivalent: "")
-    
+
     statusBarMenu.addItem(.separator())
     statusBarMenu.addItem(
         withTitle: "Quit",
         action: #selector(AppDelegate.quit),
         keyEquivalent: "q")
   }
-  
+
 
   func getCurrentLoggedInUserFromRoot() -> String? {
     var uid: uid_t = 0
     var gid: gid_t = 0
-    
+
     if let name = SCDynamicStoreCopyConsoleUser(nil, &uid, &gid) {
       return name as String;
     }
-    
+
     return nil
   }
-  
+
   func getRootPath() -> String? {
     // Since we are running from root, the current user is root so
     // I have to use this to get the actual logged in user
     let currentLoggedInUser = getCurrentLoggedInUserFromRoot();
-    
+
     if currentLoggedInUser == nil { return nil }
-    
+
     return "/Users/\(currentLoggedInUser!)/keyRemapperMac"
   }
 
@@ -173,7 +174,7 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
     if (rootPath == nil) { return }
     NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: rootPath!)
   }
-  
+
   @objc func openAboutWindow() {
           if aboutBoxWindowController == nil {
             let aboutView = AboutView();
@@ -188,7 +189,7 @@ fileprivate class AppDelegate: NSObject, NSApplicationDelegate {
     NSApp.activate(ignoringOtherApps: true)
     aboutBoxWindowController?.window?.orderFrontRegardless()
     aboutBoxWindowController?.window?.center()
-    
+
           aboutBoxWindowController?.showWindow(aboutBoxWindowController?.window)
       }
 
