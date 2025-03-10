0553eb1e25e583b8be4934fda2b763f6a54e3a71
Erwin Gaitan <erwingaitano@gmail.com>
2025-03-10 00:50:32 -0500

    chore: another try

--- mac/buildApp.sh
+++ mac/buildApp.sh
@@ -60,11 +60,9 @@ fi
 # create variables
 CERTIFICATE_PATH=$RUNNER_TEMP/build_certificate.p12
 KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
-# PP_PATH=$RUNNER_TEMP/build_pp.mobileprovision
 
-# import certificate from secrets
+# import certificate and provisioning profile from secrets
 echo -n "$BUILD_CERTIFICATE_BASE64" | base64 --decode -o $CERTIFICATE_PATH
-# echo -n "$BUILD_PROVISION_PROFILE_BASE64" | base64 --decode -o $PP_PATH
 
 # create temporary keychain
 security create-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
@@ -73,11 +71,8 @@ security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 
 # import certificate to keychain
 security import $CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+security set-key-partition-list -S apple-tool:,apple: -k "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 security list-keychain -d user -s $KEYCHAIN_PATH
-
-# apply provisioning profile
-# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-# cp $PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
 
 ###
 #######################################################################
